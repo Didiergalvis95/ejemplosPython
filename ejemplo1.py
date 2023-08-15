dc46e5c3d78654a6835f7dab6ecd0772b644e616
309 <309@academicos.cesde.edu.co>
2023-08-15 08:36:57 -0500

    feat: se crean entradas por teclado

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,4 @@
-nombreUsuario = "Didier Alexander Galvis"
+'''nombreUsuario = "Didier Alexander Galvis"
 edadUsuario = 28
 estaturaUsuario = 1.66
 esHinchaDelverde = True
@@ -8,3 +8,13 @@ print(f"su nombre es {nombreUsuario} y tiene {edadUsuario} años")
 comidasFavoritas = ["frisoles", "sushicharron", "tacos", "hamburguesa", "papas"]
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+#entradas por teclado
+lugarTragabajo=input("Digita el lugar de trabajo: ")
+print(f"Su lugar de trabajo es: {lugarTragabajo}")
+'''
+
+numeroUno = int(input("ingrese el primer numero: "))
+numeroDos = int(input("ingrese el segundo numero: "))
+total = numeroUno + numeroDos
+print(f"la suma es {numeroUno} + {numeroDos} = {total}")
